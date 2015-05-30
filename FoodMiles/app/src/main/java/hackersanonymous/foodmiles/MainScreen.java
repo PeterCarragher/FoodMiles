f32be0e5603c468e9ddd2be48393fe98edefbe53
PeterCarragher <petercarragher6@gmail.com>
2015-05-30 19:57:57 +0100

    pattern matching included, new activity initialised

--- FoodMiles/app/src/main/java/hackersanonymous/foodmiles/MainScreen.java
+++ FoodMiles/app/src/main/java/hackersanonymous/foodmiles/MainScreen.java
@@ -2,17 +2,36 @@ package hackersanonymous.foodmiles;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class MainScreen extends Activity{
 
     private Button mButton;
+    RequestTask postRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +48,6 @@ public class MainScreen extends Activity{
                 integrator.initiateScan();
             }
         });
-
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -37,16 +55,78 @@ public class MainScreen extends Activity{
         if (scanResult != null) {
             String re = scanResult.getContents();
             String str = scanResult.toString();
-            Log.d("code", re);
-            Log.d("string", str);
         }
         // else continue with any other code you need in the method
 
         String uri = "http://gepir.gs1.org/v32/xx/gtin.aspx?Lang=en-US";
-        RequestTask postRequest = new RequestTask();
-        //String result = postRequest.execute(uri);
-        //Log.d("result", result);
+        postRequest = new RequestTask();
+        postRequest.execute(uri);
+    }
 
+    class RequestTask extends AsyncTask<String, String, String> {
+
+        String result;
+
+        @Override
+        protected String doInBackground(String... uri) {
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            HttpPost httppost;
+            ArrayList<NameValuePair> postParameters;
+            httpclient = new DefaultHttpClient();
+            httppost = new HttpPost(uri[0]);
+
+            postParameters = new ArrayList<NameValuePair>();
+            postParameters.add(new BasicNameValuePair("param1", "param1_value"));
+            postParameters.add(new BasicNameValuePair("param2", "param2_value"));
+
+            try {
+                httppost.setEntity(new UrlEncodedFormEntity(postParameters));
+
+                response = httpclient.execute(httppost);
+
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Do anything with response..
+
+            Pattern pattern = Pattern.compile("[A-Z]{1,2}[0-9][0-9A-Z]?\\s?[0-9][A-Z]{2}");
+
+            Document doc = Jsoup.parse(result);
+
+            Element address = doc.select("div.adressscroll").first();
+            String divString = address.html();
+
+            Matcher matcher = pattern.matcher(divString);
+            if (matcher.find()){
+                System.out.println(matcher.group(1));
+            }
+
+
+            this.result = matcher.group(1);
+
+        }
 
     }
+
+
+
 }
