4e6705383a73ddffb9ff9172585fa426def01a56
PeterCarragher <petercarragher6@gmail.com>
2015-05-30 14:41:04 +0100

    added barcode scanner
    IT WORKS!

--- FoodMiles/app/src/main/java/hackersanonymous/foodmiles/MainScreen.java
+++ FoodMiles/app/src/main/java/hackersanonymous/foodmiles/MainScreen.java
@@ -1,36 +1,45 @@
 package hackersanonymous.foodmiles;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class MainScreen extends Activity {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        // Scanner
+
+        mButton = (Button) findViewById(R.id.assistant_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainScreen.this);
+                integrator.initiateScan();
+            }
+        });
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String re = scanResult.getContents();
+            Log.d("code", re);
         }
-        return super.onOptionsItemSelected(item);
+        // else continue with any other code you need in the method
+
     }
 }
